6f13321d5e4e59b7034a0fa742cc168112d92c10
Melissa Lowe <mel.c.lowe@gmail.com>
2021-03-06 20:54:26 -0600

    Reorganized files, saved png of ERD

---
+++ Code/Old/total_code.sql
@@ -0,0 +1,74 @@
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
+
+-- Physical ERD
+
+CREATE TABLE "Employees" (
+    "emp_no" INT   NOT NULL,
+    "emp_title_id" VARCHAR   NOT NULL,
+	"birth_data" DATE  NOT Null,
+    "first_name" VARCHAR   NOT NULL,
+    "last_name" VARCHAR   NOT NULL,
+    "sex" VARCHAR   NOT NULL,
+    "hire_date" DATE   NOT NULL,
+    CONSTRAINT "pk_Employees" PRIMARY KEY (
+        "emp_no"
+     )
+);
+
+CREATE TABLE "Departments" (
+    "dept_no" VARCHAR   NOT NULL,
+    "dept_name" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Departments" PRIMARY KEY (
+        "dept_no"
+     )
+);
+
+CREATE TABLE "Dept_manager" (
+    "dept_no" VARCHAR   NOT NULL,
+    "emp_no" INT   NOT NULL
+);
+
+CREATE TABLE "Dept_emp" (
+    "emp_no" INT   NOT NULL,
+    "dept_no" VARCHAR   NOT NULL
+);
+
+CREATE TABLE "Title" (
+    "title_id" VARCHAR   NOT NULL,
+    "title" VARCHAR   NOT NULL,
+    CONSTRAINT "pk_Title" PRIMARY KEY (
+        "title_id"
+     )
+);
+
+CREATE TABLE "Salaries" (
+    "emp_no" INT   NOT NULL,
+    "salary" INT   NOT NULL
+);
+
+ALTER TABLE "Employees" ADD CONSTRAINT "fk_Employees_emp_title_id" FOREIGN KEY("emp_title_id")
+REFERENCES "Title" ("title_id");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Dept_manager" ADD CONSTRAINT "fk_Dept_manager_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+ALTER TABLE "Dept_emp" ADD CONSTRAINT "fk_Dept_emp_dept_no" FOREIGN KEY("dept_no")
+REFERENCES "Departments" ("dept_no");
+
+ALTER TABLE "Salaries" ADD CONSTRAINT "fk_Salaries_emp_no" FOREIGN KEY("emp_no")
+REFERENCES "Employees" ("emp_no");
+
+
+select * from "Title";
+select * from "Employees";
+select * from "Salaries";
+select * from "Departments";
+select * from "Dept_manager";
+select * from "Dept_emp";
---
+++ Code/task_code.sql
@@ -0,0 +1,64 @@
+-- Homework task 1 
+--(emp_no, last_name, first_name, salary)
+
+SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
+FROM "Employees" as e
+JOIN "Salaries" as s
+ON (e.emp_no = s.emp_no);
+
+--Homework task 2 
+--(first_name, last_name, hire_date, condition: hired in 1986)
+
+select e.first_name, e.last_name, e.hire_date
+from "Employees" as e
+where hire_date >= '01/01/86';
+
+--Homework task 3 manager of each department:
+--(dept_no, dept_name, emp_no, last_name, first_name) 
+
+select d.dept_no, d.dept_name, e.emp_no, e.last_name, e.first_name
+from "Employees" as e
+join "Dept_manager" as dm
+on e.emp_no = dm.emp_no
+join "Departments" as d
+on d.dept_no = dm.dept_no;
+
+
+--Homework task 4 
+--(emp_no, last_name, first_name, dept_name)
+
+select e.emp_no, e.last_name, e.first_name, d.dept_name
+from "Employees" as e
+join "Dept_emp" as de
+on e.emp_no = de.emp_no
+join "Departments" as d
+on d.dept_no = de.dept_no;
+
+--Homework task 5
+--(first_name, last_name, sex for "Hercules", "B%")
+
+select e.first_name, e.last_name, sex
+from "Employees" as e
+where e.last_name like 'B%' AND e.first_name = 'Hercules'
+
+--Homework task 6
+--(employees in Sales, emp_no, last_name, first_name, dept_name)
+
+select e.emp_no, e.last_name, e.first_name, d.dept_name 
+from "Departments" as d, "Employees" as e
+where d.dept_name = 'Sales';
+
+--Homework task 7
+--(Employees in Sales and Development, emp_no, last_name, first_name, dept_name)
+
+select e.emp_no, e.last_name, e.first_name, d.dept_name 
+from "Departments" as d, "Employees" as e
+where d.dept_name = 'Sales' OR d.dept_name = 'Development';
+
+--Homework task 8
+--(Descending order of count of unique last names)
+
+select e.last_name, count(last_name) as name_count
+from "Employees" as e
+group by e.last_name
+order by name_count desc;
